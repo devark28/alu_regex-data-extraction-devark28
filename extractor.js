611322326b9e7e31ed98892e89a93687092ce26b
devark28 <e.shimwa1@alustudent.com>
2025-02-15 22:11:46 +0200

    Create extractor script and test simple text data

---
+++ extractor.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const [, , fileA] = process.argv;
+let data =  require('fs').readFileSync(fileA, { encoding: 'utf-8' });
+
+const matchers = {
+  emails: RegExp(/\w+[\w\.]*@\w+[\w\.]+\.\w+/gi),
+  phones: RegExp(/\(?\d{3}\)?[\.\-\s]\d{3}[\.\-\s]\d{4}/g),
+  hashtags: RegExp(/#\w+/gi),
+  cards: RegExp(/(\d{4}[\-\s]){3}\d{4}/g),
+};
+
+let results = {
+  emails: [],
+  phones: [],
+  hashtags: [],
+  cards: [],
+};
+
+for(matcher in matchers){
+  results[matcher] = [...data.match(matchers[matcher])];
+}
+
+console.log(JSON.stringify(results, null, 2));
